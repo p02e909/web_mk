33763e4ea19842cb5a5c89a829ebbe3d069ea756
nhattao <p02e909@gmail.com>
2018-12-02 17:28:26 +0700

    Add files via upload

---
+++ auto_test.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+url = 'https://facebook.com'
+with open('pass.txt', 'rt') as f:
+    pwd = f.read()
+
+
+browser = webdriver.Firefox()
+browser.get(url)
+
+
+username = browser.find_element_by_id('email')
+username.send_keys('p02e909@gmail.com')
+
+password = browser.find_element_by_id('pass')
+password.send_keys('asdf')
+
+sleep(2)
+submit = browser.find_element_by_id('u_0_2')
+submit.click()
+
+#---------------------------------------------------
+
+password_when_fail = browser.find_element_by_id('pass')
+password_when_fail.send_keys(pwd)
+
+submit = browser.find_element_by_id('loginbutton')
+submit.click()
+
+browser.quit()
