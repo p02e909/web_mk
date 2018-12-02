bbd46e6aa8bff56453d7232ad82901289a47f354
nhattao <p02e909@gmail.com>
2018-12-02 18:09:04 +0700

    Update auto_test.py

--- auto_test.py
+++ auto_test.py
@@ -3,31 +3,49 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
+ids = []
 url = 'https://facebook.com'
 with open('pass.txt', 'rt') as f:
     pwd = f.read()
-
+#---------------------------------------------------
 
 browser = webdriver.Firefox()
 browser.get(url)
-
-
-username = browser.find_element_by_id('email')
-username.send_keys('p02e909@gmail.com')
-
-password = browser.find_element_by_id('pass')
-password.send_keys('asdf')
-
-sleep(2)
-submit = browser.find_element_by_id('u_0_2')
-submit.click()
-
 #---------------------------------------------------
 
-password_when_fail = browser.find_element_by_id('pass')
-password_when_fail.send_keys(pwd)
 
-submit = browser.find_element_by_id('loginbutton')
-submit.click()
+def learn():
+    username = browser.find_element_by_id('email')
+    username.send_keys('p02e909@gmail.com')
 
-browser.quit()
+    password = browser.find_element_by_id('pass')
+    password.send_keys('asdf')
+
+    sleep(2)
+    submit = browser.find_element_by_id('u_0_2')
+    submit.click()
+#---------------------------------------------------
+
+    password_when_fail = browser.find_element_by_id('pass')
+    password_when_fail.send_keys(pwd)
+
+    submit = browser.find_element_by_id('loginbutton')
+    submit.click()
+#---------------------------------------------------
+
+
+def test_UI(ids):
+    for elem in ids:
+        clicker = browser.find_element_by_id(elem)
+        sleep(3)
+        print("{}: done".format(elem))
+
+
+def main():
+    test_UI(ids)
+    print("ALL: done")
+    #browser.quit()
+
+
+if __name__ == "__main__":
+    main()
